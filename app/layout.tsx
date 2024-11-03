8b2d94669d6c315876f216b3a8b80f5def74b1cd
Chust3r <kriptoortega@gmail.com>
2024-11-02 19:38:33 -0600

    Add euclid flex font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,9 @@ import localFont from 'next/font/local'
 import './globals.css'
 import { cn } from '~lib/utils'
 
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-})
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
+const euclidFlex = localFont({
+	src: './fonts/EuclidFlexRegular.ttf',
+	variable: '--font-euclid-flex',
 })
 
 interface Props {
@@ -21,13 +15,7 @@ interface Props {
 function Layout({ children }: Props) {
 	return (
 		<html lang='en'>
-			<body
-				className={cn(
-					'antialiased layout',
-					geistSans.variable,
-					geistMono.variable
-				)}
-			>
+			<body className={cn('antialiased layout', euclidFlex)}>
 				{children}
 			</body>
 		</html>
